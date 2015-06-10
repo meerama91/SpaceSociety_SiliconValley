b09bbe85a3c3649b4c82bef5ea907e6b18f8e5c0
Meera Mali <mmee91@gmail.com>
2015-06-09 21:11:28 -0700

    Changes made -
    1) AndroidManifest cleanup.
    2) Add AndroidManifest callback for onClick()

--- app/src/main/java/com/spacesociety/FirstScreen.java
+++ app/src/main/java/com/spacesociety/FirstScreen.java
@@ -8,25 +8,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class FirstScreen extends Activity {
 
-    private Button mExplore;
+    public void exploreMore(View v) {
+        startActivity(new Intent(FirstScreen.this, MenuGrid.class));
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_screen);
-        mExplore = (Button) findViewById(R.id.explore);
-        mExplore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FirstScreen.this, MenuGrid.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 
 }
